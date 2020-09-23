384e3d5983cccaf33d24eed8eeb499c2de456806
Pénélope PRINGALLE <penelopepringalle@gmail.com>
2020-09-23 19:37:32 +0200

    creation vue massage, composants

---
+++ src/views/Massage.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="Massage">
+        <p>Ceci est la page massage</p>
+      <Carousel/>
+    </div>
+</template>
+
+<script>
+import Carousel from '@/components/Carousel.vue'
+export default {
+    name: "Massage",
+    components: {
+        Carousel
+    }
+}
+</script>

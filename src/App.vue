51b52780d08cf84ba93d578ebe398e5a13bc3350
Sylvain Gounon <sylvain.gounon@icloud.com>
2020-09-24 09:16:54 +0200

    init app

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,25 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <Navbar />
+    <router-view />
   </div>
 </template>
 
+<script>
+import Navbar from "@/components/Navbar.vue";
+
+export default {
+  components: {
+    Navbar,
+  },
+  mounted() {},
+};
+</script>
+
 <style lang="scss">
+@import "@/assets/css/styles.scss";
 #app {
+  padding-top: 200px;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -17,16 +27,18 @@
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
+* {
+  box-sizing: border-box;
+}
+body {
+  margin: 0;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+ul {
+  padding: 0;
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+a {
+  text-decoration: none;
 }
 </style>
